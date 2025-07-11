e8356546a162a5cf2d525dca809eebe8fc0b3cfb
Harshit-24122 <135972800+Harshit-24122@users.noreply.github.com>
2025-07-11 21:22:57 +0530

    final commit message

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds){
 
 async function getSongs(folder){
     currentFolder = folder;
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
     let response = await a.text();
     // console.log(response);
 
@@ -69,8 +69,8 @@ const playMusic = (track, pause = false) =>{
 }
 
 async function displayAlbums(){
-    let a = await fetch(`http://127.0.0.1:5500/songs/`);
-    let response = await a.json();
+    let a = await fetch(`http://127.0.0.1:3000/songs/`);
+    let response = await a.text();
     console.log(a);
     console.log(response);
     let div = document.createElement("div");
@@ -83,7 +83,7 @@ async function displayAlbums(){
         if(e.href.includes("/songs")){
             // console.log(e.href);
             let folder = e.href.split("/").slice(-2)[0];
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
             let response = await a.json();
             console.log(response);
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="folder1" class="card">
